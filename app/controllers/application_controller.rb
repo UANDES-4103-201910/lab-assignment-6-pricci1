8f50c00d5d89476981b18b47ab8b4182eaa3e5a7
Pedro <priccis@gmail.com>
2019-04-20 22:37:53 -0400

    User log in and session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  include SessionsHelper
 
   def index
 
   end
 
   def current_user
-	#complete this method
+    if session[:user_id]
+      @current_user ||= User.find_by(id: session[:user_id])
+    end
   end
 
   def is_user_logged_in?
-	#complete this method
-  	logged_in = false
-	if logged_in then true else redirect_to root_path end 
+    logged_in = !current_user.nil?
+    if logged_in then true else redirect_to root_path end
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,10 +3,19 @@ class SessionsController < ApplicationController
 	end
 
 	def create
-		#complete this method
+		user = User.find_by(email: params[:session][:email].downcase)
+		if user && user.password == params[:session][:password]
+			log_in user
+			debugger
+			redirect_to user
+		else
+			flash.now[:danger] = 'Invalid email and/or password'
+			render 'new'
+		end
 	end
 
 	def destroy
-		#complete this method
+		session.delete(:user_id)
+		@current_user = nil
 	end
 end
