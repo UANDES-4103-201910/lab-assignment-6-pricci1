94831dbb18d3ad3bf5ca9e128fb1e938b79b5941
Pedro <priccis@gmail.com>
2019-04-20 21:25:57 -0400

    All create new user

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,9 +1,22 @@
 class RegistrationsController < ApplicationController
-	def new
-	
-	end
+  def new
+		@user = User.new
+  end
 
-	def create
-	    #complete this method
-	end
+  def create
+    @user = User.new(user_params)
+		if @user.save
+			flash[:success] = "User created successfuly"
+			redirect_to '/'
+		else
+			render 'new'
+		end
+  end
+
+  private
+
+  def user_params
+    params.require(:registrations).permit(:email, :name, :last_name,
+                                           :password, :phone)
+  end
 end
